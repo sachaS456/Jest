6361a1e1184f3858d9ec1c7891f61f2e98560a32
himb0004 <sacha.himber@etudiant.univ-reims.fr>
2025-10-09 14:13:24 +0200

    CardEffect method

--- JestGame/src/CardEffect.java
+++ JestGame/src/CardEffect.java
@@ -3,43 +3,119 @@ import java.util.ArrayList;
 public enum CardEffect {
     HIGHEST {
         @Override
-        public Player CheckEffect(int value, Color color, Player[] players) {
-            return null;
+        public Player CheckEffect(int value, Sign sign, Player[] players) {
+            Player winner = null;
+            SuitCard higher = null;
+            for(Player player : players){
+                ArrayList<Card> jest = player.getJest();
+                for (Card card : jest) {
+                    if (card instanceof SuitCard suitCard) {
+                        if (suitCard.getSign() == sign && (higher == null || suitCard.getValue() > higher.getValue())) {
+                            higher = suitCard;
+                            winner = player;
+                        }
+                    }
+                }
+            }
+
+            return winner;
         }
     },
     LOWEST {
         @Override
-        public Player CheckEffect(int value, Color color, Player[] players) {
-            return null;
+        public Player CheckEffect(int value, Sign sign, Player[] players) {
+            Player winner = null;
+            SuitCard higher = null;
+            for(Player player : players){
+                ArrayList<Card> jest = player.getJest();
+                for (Card card : jest) {
+                    if (card instanceof SuitCard suitCard) {
+                        if (suitCard.getSign() == sign && (higher == null || suitCard.getValue() < higher.getValue())) {
+                            higher = suitCard;
+                            winner = player;
+                        }
+                    }
+                }
+            }
+
+            return winner;
         }
     },
     MAJORITY {
         @Override
-        public Player CheckEffect(int value, Color color, Player[] players) {
-            return null;
+        public Player CheckEffect(int value, Sign sign, Player[] players) {
+            Player winner = null;
+            int countMajority = 0;
+            for(Player player : players){
+                ArrayList<Card> jest = player.getJest();
+                int countCardInJest = 0;
+                for (Card card : jest) {
+                    if (card instanceof SuitCard suitCard) {
+                        if (suitCard.getValue() == value) {
+                            countCardInJest++;
+                        }
+                    }
+                }
+
+                if (countMajority < countCardInJest){
+                    winner = player;
+                    countMajority = countCardInJest;
+                }
+            }
+
+            return winner;
         }
     },
     JOKER {
         @Override
-        public Player CheckEffect(int value, Color color, Player[] players) {
+        public Player CheckEffect(int value, Sign sign, Player[] players) {
+            for(Player player : players){
+                if (player.hasJokerCard()) {
+                    return player;
+                }
+            }
+
             return null;
         }
     },
     BEST_JEST {
         @Override
-        public Player CheckEffect(int value, Color color, Player[] players) {
-            return null;
+        public Player CheckEffect(int value, Sign sign, Player[] players) {
+            Player winner = null;
+            int besJest = 0;
+            for(Player player : players){
+                int playerJest = Game.getJestPoints(player);
+                if (playerJest >= besJest){
+                    winner = player;
+                    besJest = playerJest;
+                }
+            }
+
+            return winner;
         }
     },
     BEST_JEST_WITHOUT_JOKER {
         @Override
-        public Player CheckEffect(int value, Color color, Player[] players) {
-            return null;
+        public Player CheckEffect(int value, Sign sign, Player[] players) {
+            Player winner = null;
+            int besJest = 0;
+            for(Player player : players){
+                if (player.hasJokerCard()){
+                    continue;
+                }
+                int playerJest = Game.getJestPoints(player);
+                if (playerJest >= besJest){
+                    winner = player;
+                    besJest = playerJest;
+                }
+            }
+
+            return winner;
         }
     };
 
 
-    public abstract Player CheckEffect(int value, Color color, Player[] players);
+    public abstract Player CheckEffect(int value, Sign sign, Player[] players);
 
     /* public Joueur CheckCondition(Player[] players) { a mettre dans game
 
