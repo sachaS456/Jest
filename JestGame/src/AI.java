4b44912779927a58f5e761919c4c0d796ae680cd
Gatien <gatien.gen51@gmail.com>
2025-10-13 17:59:50 +0200

    Make AI choose a strategy depending on his visible cards of his Jest

--- JestGame/src/AI.java
+++ JestGame/src/AI.java
@@ -1,18 +1,67 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Random;
 
 public class AI extends Player {
-    private final IPlayStrategy strategy;
+    private IPlayStrategy strategy;
 
     public AI(String name) {
         super(name);
         this.strategy = new RandomStrategy();
     }
 
+    private Card[] getVisibleCards()
+    {
+        ArrayList<Card> visibleCards = new ArrayList<>();
+        for (Card card : this.getOffer()) {
+            if (card.isVisible()) {
+                visibleCards.add(card);
+            }
+        }
+        return visibleCards.toArray(new Card[0]);
+    }
+
+    private int countCardsWithSign(Sign sign)
+    {
+        int count = 0;
+        for (Card card : this.getOffer()) {
+            if (card instanceof SuitCard && ((SuitCard) card).getSign() == sign) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean hasVisibleJoker()
+    {
+        for (Card card : this.getOffer()) {
+            if (card instanceof JokerCard && card.isVisible()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     @Override
     public int makeChoice(int min, int max, ArrayList<Card> cards, boolean isHidingCard) {
+        if(hasVisibleJoker() && countCardsWithSign(Sign.HEARTH) >= 2 || countCardsWithSign(Sign.HEARTH) >= 3){
+            this.strategy = new RiskyStrategy();
+        }
+        else if(!hasVisibleJoker()){
+            this.strategy = new RandomStrategy();
+        }else {
+            this.strategy = new SafeStrategy();
+        }
         return this.strategy.makeChoice(min, max, cards, isHidingCard);
+    }
+
+    public void setRandomStrategy(IPlayStrategy strategy) {
+        Random randomNumbers = new Random();
+        int stategyId = randomNumbers.nextInt(0, 3);
+        switch (stategyId) {
+            case 0 -> this.strategy = new RandomStrategy();
+            case 1 -> this.strategy = new SafeStrategy();
+            case 2 -> this.strategy = new RiskyStrategy();
+        }
     }
 }
--- JestGame/src/SafeStrategy.java
+++ JestGame/src/SafeStrategy.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class SafeStrategy implements IPlayStrategy {
     @Override
     public int makeChoice(int min, int max, ArrayList<Card> cards, boolean isHidingCard) {
-        // if the AI have to hide a card he will try to hide Spades or Clubs
+        // if the AI has to hide a card he will try to hide Spades or Clubs
         if(isHidingCard){
             if(cards.getFirst() instanceof SuitCard && (((SuitCard) cards.getFirst()).getSign() == Sign.SPIKE || ((SuitCard) cards.getFirst()).getSign() == Sign.CLOVER)){
                 return 0;
@@ -11,7 +11,7 @@ public class SafeStrategy implements IPlayStrategy {
                 return 1;
             }
         }else {
-            // if AI have to pick a card he will try to pick Spades or Clubs
+            // if AI has to pick a card he will try to pick Spades or Clubs
             for(int i = 0; i < cards.size(); i++){
                 if(cards.get(i) instanceof SuitCard && ((SuitCard) cards.get(i)).isVisible() && (((SuitCard) cards.get(i)).getSign() == Sign.SPIKE || ((SuitCard) cards.get(i)).getSign() == Sign.CLOVER)){
                     return i;
