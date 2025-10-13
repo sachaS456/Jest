b8d9fb2f4fcf87daa118a33e8df6b43ba9757f09
Gatien <gatien.gen51@gmail.com>
2025-10-13 17:27:08 +0200

    Add Risky strategy

---
+++ JestGame/src/RiskyStrategy.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class RiskyStrategy implements IPlayStrategy {
+
+    @Override
+    public int makeChoice(int min, int max, ArrayList<Card> cards, boolean isHidingCard) {
+        // if the AI have to hide a card he will try to hide Joker or Hearth
+        if(isHidingCard){
+            if(cards.getFirst() instanceof SuitCard && (((SuitCard) cards.getFirst()).getSign() == Sign.HEARTH) || cards.getFirst() instanceof JokerCard){
+                return 0;
+            }else {
+                return 1;
+            }
+        }else {
+            // if AI have to pick a card he will try to pick Joker or Hearth
+            for(int i = 0; i < cards.size(); i++){
+                if(cards.get(i) instanceof SuitCard && ((SuitCard) cards.get(i)).isVisible() && (((SuitCard) cards.get(i)).getSign() == Sign.HEARTH || cards.get(i) instanceof JokerCard)){
+                    return i;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
